a11cad2f21572859f43dfc76fb04e3a5e223a9bc
Yuchen Wu <cheneyuwu@gmail.com>
2020-09-06 21:47:20 -0400

    Port path planning package from vtr2.2

---
+++ ros2/src/vtr_path_planning/include/vtr_path_planning/simple_planner.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <vtr_path_planning/planning_interface.hpp>
+
+namespace vtr {
+namespace path_planning {
+
+template <class GRAPH>
+class SimplePlanner : public PlanningInterface {
+ public:
+  PTR_TYPEDEFS(SimplePlanner)
+  using GraphBasePtr = decltype(std::declval<GRAPH>().getManualSubgraph());
+
+  SimplePlanner(const typename GRAPH::Ptr &graph) : graph_(graph) {
+    updatePrivileged();
+  }
+
+  virtual PathType path(const VertexId &from, const VertexId &to);
+  virtual PathType path(const VertexId &from, const VertexId::List &to,
+                        std::list<uint64_t> *idx = nullptr);
+
+  virtual void updatePrivileged();
+
+  virtual double cost(const VertexId &) { return 1; }
+  virtual double cost(const EdgeId &) { return 1; }
+
+  virtual EvalPtr cost() {
+    return asrl::pose_graph::Eval::Weight::Const::MakeShared(1, 1);
+  }
+
+ private:
+  PathType _path(const VertexId &from, const VertexId &to);
+
+  typename GRAPH::Ptr graph_;
+
+  GraphBasePtr privileged_;
+};
+
+}  // namespace path_planning
+}  // namespace vtr
+
+#include <vtr_path_planning/simple_planner.inl>
