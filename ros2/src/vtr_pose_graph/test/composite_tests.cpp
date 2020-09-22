cf246ad6ee8445cd5106cd911dc533022933a82e
Yuchen Wu <cheneyuwu@gmail.com>
2020-09-22 15:36:19 -0400

    enable composite test

--- ros2/src/vtr_pose_graph/test/composite_tests.cpp
+++ ros2/src/vtr_pose_graph/test/composite_tests.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <random>
 
+#include <vtr_common/timing/time_utils.hpp>
 #include <vtr_logging/logging_init.hpp>
 #include <vtr_pose_graph/evaluator/composite.hpp>
 #include <vtr_pose_graph/evaluator/mask_evaluator.hpp>
@@ -14,6 +15,7 @@
 #include <vtr_pose_graph/index/rc_graph/rc_graph.hpp>
 
 namespace fs = std::filesystem;
+using namespace vtr::common;
 using namespace vtr::pose_graph;
 
 using SimpleVertex = uint64_t;
@@ -34,13 +36,11 @@ class CompositeTest : public ::testing::Test {
         irnd_(std::bind(std::uniform_int_distribution<int64_t>{0, 1000},
                         std::mt19937(std::random_device{}()))),
         drnd_(std::bind(std::uniform_real_distribution<double>{0.f, 100.f},
-                        std::mt19937(std::random_device{}()))),
-        robot_id_(0) {}
+                        std::mt19937(std::random_device{}()))) {}
 
   ~CompositeTest() override {}
 
   void SetUp() override {
-#if 0
     /* Create the following graph
      * R0: 0 --- 1 --- 2
      *       \
@@ -55,20 +55,17 @@ class CompositeTest : public ::testing::Test {
      * R4: 0 --- 1 --- 2
      */
 
-    using namespace vtr::common;
     auto stamp = timing::clock::now();
 
     // add a graph with 5 runs and 3 vertices per run.
     for (int idx = 0; idx < 5; ++idx) {
-      // Create the robochunk directories
+      graph_->addRun(0);
 
-      (void)graph_->addRun(robot_id_);
-
-      graph_->addVertex(timing::toRobochunk(stamp - timing::hours(idx)));
-      graph_->addVertex(
-          timing::toRobochunk(stamp - timing::hours(idx) + timing::seconds(5)));
-      graph_->addVertex(timing::toRobochunk(stamp - timing::hours(idx) +
-                                            timing::seconds(10)));
+      graph_->addVertex(timing::toRosTimestamp(stamp - timing::hours(idx)));
+      graph_->addVertex(timing::toRosTimestamp(stamp - timing::hours(idx) +
+                                               timing::seconds(5)));
+      graph_->addVertex(timing::toRosTimestamp(stamp - timing::hours(idx) +
+                                               timing::seconds(10)));
       graph_->addEdge(VertexId(idx, 0), VertexId(idx, 1));
       graph_->addEdge(VertexId(idx, 1), VertexId(idx, 2));
     }
@@ -78,7 +75,6 @@ class CompositeTest : public ::testing::Test {
     graph_->addEdge(VertexId(2, 2), VertexId(1, 2), Spatial);
     graph_->addEdge(VertexId(3, 1), VertexId(2, 1), Spatial);
     graph_->addEdge(VertexId(4, 2), VertexId(3, 2), Spatial);
-
     // set the edge's transform to something special;
     auto edge_map = graph_->edges();
     for (auto itr = edge_map->begin(); itr != edge_map->end(); ++itr) {
@@ -90,81 +86,49 @@ class CompositeTest : public ::testing::Test {
     }
 
     cgraph_.reset(new CompositeGraph<RCGraph>(graph_));
-#endif
   }
 
   void TearDown() override {}
 
-  SimpleVertex rndSimpleVertex() { return SimpleVertex(irnd_()); }
+  // SimpleVertex rndSimpleVertex() { return SimpleVertex(irnd_()); }
 
-  SimpleEdge rndSimpleEdge() { return SimpleEdge(irnd_(), irnd_()); }
+  // SimpleEdge rndSimpleEdge() { return SimpleEdge(irnd_(), irnd_()); }
 
-  VertexId rndVertexId() { return VertexId(irnd_(), irnd_()); }
+  // VertexId rndVertexId() { return VertexId(irnd_(), irnd_()); }
 
-  EdgeId rndEdgeId(const EdgeId::Type& type) {
-    if (type == EdgeId::Type::Temporal) {
-      auto run_id = irnd_();
-      return EdgeId(SimpleVertex(VertexId(run_id, irnd_())),
-                    SimpleVertex(VertexId(run_id, irnd_())), type);
-    } else {
-      return EdgeId(SimpleVertex(rndVertexId()), SimpleVertex(rndVertexId()),
-                    type);
-    }
-  }
+  // EdgeId rndEdgeId(const EdgeId::Type& type) {
+  //   if (type == EdgeId::Type::Temporal) {
+  //     auto run_id = irnd_();
+  //     return EdgeId(SimpleVertex(VertexId(run_id, irnd_())),
+  //                   SimpleVertex(VertexId(run_id, irnd_())), type);
+  //   } else {
+  //     return EdgeId(SimpleVertex(rndVertexId()), SimpleVertex(rndVertexId()),
+  //                   type);
+  //   }
+  // }
 
  public:
   RCGraph::Ptr graph_;
   typename CompositeGraph<RCGraph>::Ptr cgraph_;
   IntRandType irnd_;
   DoubleRandType drnd_;
-  int robot_id_;
 };
 
-#if 0
-TEST_CASE_METHOD(CompositeTest, "Weight Evaluator", "[weight]") {
+TEST_F(CompositeTest, WeightEvaluator) {
   auto drnd = std::bind(std::uniform_real_distribution<double>{0, 100},
                         std::mt19937(std::random_device{}()));
 
-  SECTION("Constant", "[const]") {
-    double eweight = drnd(), vweight = drnd();
-    auto eval1 = Eval::Weight::Const::MakeShared(eweight, vweight);
-    auto eval2 = Eval::Composite<Eval::Weight::Const>::MakeDirect(
+  // SECTION("Constant", "[const]")
+  {
+    auto eweight = drnd(), vweight = drnd();
+    auto eval1 = eval::Weight::Const::MakeShared(eweight, vweight);
+    auto eval2 = eval::Composite<eval::Weight::Const>::MakeDirect(
         cgraph_.get(), eweight, vweight);
 
-    CHECK_NOTHROW(eval1->setGraph(graph_.get()));
+    eval1->setGraph(graph_.get());
 
-    auto res1 = graph_->dijkstraSearch(VertexId(0, 2), VertexId(4, 0),
-    eval1); auto res2 = cgraph_->dijkstraSearch(VertexId(0, 2), VertexId(4,
-    0), eval2)
-                    ->flatten();
-
-    auto it1 = res1->begin(VertexId(0, 2));
-    auto it2 = res2->begin(VertexId(0, 2));
-
-    while (it1 != res1->end() && it2 != res2->end()) {
-      CHECK((it2->v()->id() == it1->v()->id()));
-      ++it1;
-      ++it2;
-    }
-
-    CHECK((it1 == res1->end() && it2 == res2->end()));
-  }
-
-  SECTION("Time", "[time]") {
-    using namespace asrl::common;
-
-    auto evalTime = timing::clock::now() - timing::minutes(30);
-    auto eval1 = Eval::Weight::TimeDelta<RCGraph>::Direct::MakeShared(
-        Eval::Weight::TimeDeltaConfig(evalTime));
-    auto eval2 =
-        Eval::Composite<Eval::Weight::TimeDelta<RCGraph>::Direct>::MakeDirect(
-            cgraph_.get());
-
-    CHECK_NOTHROW(eval1->setGraph(graph_.get()));
-
-    auto res1 = graph_->dijkstraSearch(VertexId(0, 2), VertexId(4, 0),
-    eval1); auto res2 = cgraph_->dijkstraSearch(VertexId(0, 2), VertexId(4,
-    0), eval2)
+    auto res1 = graph_->dijkstraSearch(VertexId(0, 2), VertexId(4, 0), eval1);
+    auto res2 = cgraph_->dijkstraSearch(VertexId(0, 2), VertexId(4, 0), eval2)
                     ->flatten();
 
     auto it1 = res1->begin(VertexId(0, 2));
@@ -172,19 +136,49 @@ TEST_CASE_METHOD(CompositeTest, "Weight Evaluator", "[weight]") {
 
     while (it1 != res1->end() && it2 != res2->end()) {
       LOG(INFO) << it1->v()->id() << ", " << it2->v()->id();
-      CHECK((it2->v()->id() == it1->v()->id()));
+      EXPECT_EQ(it2->v()->id(), it1->v()->id());
       ++it1;
       ++it2;
     }
 
-    CHECK((it1 == res1->end() && it2 == res2->end()));
+    EXPECT_EQ(it1, res1->end());
+    EXPECT_EQ(it2, res2->end());
+  }
+
+  // SECTION("Time", "[time]")
+  {
+    auto eval_time = timing::clock::now() - timing::minutes(30);
+    auto eval1 = eval::Weight::TimeDelta<RCGraph>::Direct::MakeShared(
+        eval::Weight::TimeDeltaConfig(eval_time));
+    auto eval2 =
+        eval::Composite<eval::Weight::TimeDelta<RCGraph>::Direct>::MakeDirect(
+            cgraph_.get());
+
+    eval1->setGraph(graph_.get());
+
+    auto res1 = graph_->dijkstraSearch(VertexId(0, 2), VertexId(4, 0), eval1);
+    auto res2 = cgraph_->dijkstraSearch(VertexId(0, 2), VertexId(4, 0), eval2)
+                    ->flatten();
+
+    auto it1 = res1->begin(VertexId(0, 2));
+    auto it2 = res2->begin(VertexId(0, 2));
+
+    while (it1 != res1->end() && it2 != res2->end()) {
+      LOG(INFO) << it1->v()->id() << ", " << it2->v()->id();
+      EXPECT_EQ(it2->v()->id(), it1->v()->id());
+      ++it1;
+      ++it2;
+    }
+
+    EXPECT_EQ(it1, res1->end());
+    EXPECT_EQ(it2, res2->end());
 
     LOG(INFO) << VertexId(0, 0) << ": " << eval1->at(VertexId(0, 0));
     LOG(INFO) << VertexId(0, 1) << ": " << eval1->at(VertexId(0, 1));
     LOG(INFO) << VertexId(0, 2) << ": " << eval1->at(VertexId(0, 2));
   }
 }
-#endif
+
 int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
