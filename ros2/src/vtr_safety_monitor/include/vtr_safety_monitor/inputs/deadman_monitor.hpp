3b03b8ca06a32319639f877ae3481de42fb674c5
ben-congram <ben.congram@robotics.utias.utoronto.ca>
2021-04-08 17:02:51 -0400

    Start porting deadman monitor

--- ros2/src/vtr_safety_monitor/include/vtr_safety_monitor/inputs/deadman_monitor.hpp
+++ ros2/src/vtr_safety_monitor/include/vtr_safety_monitor/inputs/deadman_monitor.hpp
@@ -1,54 +1,48 @@
-#ifndef ASRL_DEADMAN_MONITOR_HPP
-#define ASRL_DEADMAN_MONITOR_HPP
+#pragma once
 
-#include <ros/ros.h>
-#include <std_msgs/String.h>
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <sensor_msgs/msg/joy.hpp>
 
-#include <sensor_msgs/Joy.h>
+#include <vtr_safety_monitor/base/safety_monitor_input_base.hpp>
+
 
 enum FOLLOWING_MODE {
-    AUTO,
-    MANUAL
+  AUTO,
+  MANUAL
 };
 
-namespace asrl {
-  namespace safetyMonitor {
+namespace vtr {
+namespace safety_monitor {
 
-    class deadman_monitor_input : public safetyMonitorInput
-    {
-    public :
-        deadman_monitor_input(ros::NodeHandle nh);
-        void gamepadCallback(const sensor_msgs::Joy::Ptr & msg);
-	    const static int axis_array[16];
-	    const static int value_array[16];
+class DeadmanMonitorInput : public SafetyMonitorInput {
+ public :
+  DeadmanMonitorInput(ros::NodeHandle nh);
+  void gamepadCallback(const sensor_msgs::msg::Joy::Ptr &msg);
+  const static int axis_array[16];
+  const static int value_array[16];
 
-    private :
-        ros::Subscriber gamepadSubscriber_;
+ private :
+  ros::Subscriber gamepadSubscriber_;
 
-        int deadman_button_index_;
-        int pause_button_index_;
-        bool deadman_pressed_;
-		FOLLOWING_MODE followingMode_;
+  int deadman_button_index_;
+  int pause_button_index_;
+  bool deadman_pressed_;
+  FOLLOWING_MODE followingMode_;
 
-		int deadman_monitor;
+  int deadman_monitor;
 
-        // the state of the cheat code.
-		int codeState_;
-		// the timestamp when hte last correct value in the code sequence was added.
-		ros::Time timeofLastCodeSequence_;
+  // the state of the cheat code.
+  int codeState_;
+  // the timestamp when hte last correct value in the code sequence was added.
+  ros::Time timeofLastCodeSequence_;
 
-		// The state when the cheat code is active.
-	    static const int CHEAT_CODE_ACTIVATED=7777;
+  // The state when the cheat code is active.
+  static const int CHEAT_CODE_ACTIVATED = 7777;
 
-	    void checkCodeState(const sensor_msgs::Joy::Ptr & msg);
+  void checkCodeState(const sensor_msgs::msg::Joy::Ptr &msg);
 
+};
 
-    };
-
-  } // safetyLayer
-} // asrl
-
-#include <safety_monitor/implementation/deadman_monitor.cpp>
-
-
-#endif  // ASRL_DEADMAN_MONITOR_HPP
+} // safety_monitor
+} // vtr
--- ros2/src/vtr_safety_monitor/src/inputs/deadman_monitor.cpp
+++ ros2/src/vtr_safety_monitor/src/inputs/deadman_monitor.cpp
@@ -1,36 +1,37 @@
-#if 0   // not ported yet
-//#include <asrl/diagnostics/Diagnostics.hpp>
+#include <vtr_safety_monitor/inputs/deadman_monitor.hpp>
 
-const int asrl::safetyMonitor::deadman_monitor_input::axis_array[] =  {7,7,7,7, 7,7, 7,7,6,6, 6,6,6,6, 6,6};
-const int asrl::safetyMonitor::deadman_monitor_input::value_array[] = {1,0,1,0,-1,0,-1,0,1,0,-1,0,1,0,-1,0};
+namespace vtr {
+namespace safety_monitor {
+
+const int DeadmanMonitorInput::axis_array[] = {7, 7, 7, 7, 7, 7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6};
+const int DeadmanMonitorInput::value_array[] = {1, 0, 1, 0, -1, 0, -1, 0, 1, 0, -1, 0, 1, 0, -1, 0};
 
 // Create the monitor
-asrl::safetyMonitor::deadman_monitor_input::deadman_monitor_input(ros::NodeHandle nh) :
-  safetyMonitorInput(nh),
-  followingMode_(MANUAL),
-  codeState_(0)
-{
+DeadmanMonitorInput::DeadmanMonitorInput(ros::NodeHandle nh) :
+    safetyMonitorInput(nh),
+    followingMode_(MANUAL),
+    codeState_(0) {
 
-    signal_monitors.clear();
-    signalMonitor empty_signal_monitor(nh);
+  signal_monitors.clear();
+  signalMonitor empty_signal_monitor(nh);
 
-    /********************
-    // Initialize Message Subscriptions
-    ********************/
-    gamepadSubscriber_ = nodeHandle_.subscribe("in/joy",1,&deadman_monitor_input::gamepadCallback,this);
+  /********************
+  // Initialize Message Subscriptions
+  ********************/
+  gamepadSubscriber_ = nodeHandle_.subscribe("in/joy", 1, &DeadmanMonitorInput::gamepadCallback, this);
 
-    /********************
-    // Initialize DEADMAN monitor
-    ********************/
-    signal_monitors.push_back(empty_signal_monitor);
-    deadman_monitor = signal_monitors.size()-1;
-    double msg_timeout = 1;
-    signal_monitors[deadman_monitor].initialize_type(DISCRETE_MONITOR);
-    signal_monitors[deadman_monitor].initialize("Deadman Monitor", msg_timeout);
-    asrl::rosutil::param<int>(nodeHandle_, "base/deadman_button_index", deadman_button_index_, 1);
+  /********************
+  // Initialize DEADMAN monitor
+  ********************/
+  signal_monitors.push_back(empty_signal_monitor);
+  deadman_monitor = signal_monitors.size() - 1;
+  double msg_timeout = 1;
+  signal_monitors[deadman_monitor].initialize_type(DISCRETE_MONITOR);
+  signal_monitors[deadman_monitor].initialize("Deadman Monitor", msg_timeout);
+  asrl::rosutil::param<int>(nodeHandle_, "base/deadman_button_index", deadman_button_index_, 1);
 
-    // Initialize the gamepad callback
-    timeofLastCodeSequence_ = ros::Time::now();
+  // Initialize the gamepad callback
+  timeofLastCodeSequence_ = ros::Time::now();
 
 }
 
@@ -38,89 +39,89 @@ asrl::safetyMonitor::deadman_monitor_input::deadman_monitor_input(ros::NodeHandl
 // Define Message Callback Functions
 ********************/
 
-void asrl::safetyMonitor::deadman_monitor_input::gamepadCallback(const sensor_msgs::Joy::Ptr & msg){
+void DeadmanMonitorInput::gamepadCallback(const sensor_msgs::msg::Joy::Ptr &msg) {
 
-    signal_monitors[deadman_monitor].register_msg_but_no_status_change();
+  signal_monitors[deadman_monitor].register_msg_but_no_status_change();
 
-    bool pressed = msg->buttons[deadman_button_index_] != 0;
+  bool pressed = msg->buttons[deadman_button_index_] != 0;
 
-    if (signal_monitors[deadman_monitor].get_desired_action() == PAUSE){
+  if (signal_monitors[deadman_monitor].get_desired_action() == PAUSE) {
 
-        if (followingMode_ == AUTO || pressed){
-            signal_monitors[deadman_monitor].set_monitor_desired_action(CONTINUE);
-        } else {
-            signal_monitors[deadman_monitor].set_monitor_desired_action(PAUSE);
+    if (followingMode_ == AUTO || pressed) {
+      signal_monitors[deadman_monitor].set_monitor_desired_action(CONTINUE);
+    } else {
+      signal_monitors[deadman_monitor].set_monitor_desired_action(PAUSE);
+    }
+
+    // check the cheat code state
+    checkCodeState(msg);
+    // if the cheat code has been activated, then put the path tracker into auto mode.
+    if (codeState_ == CHEAT_CODE_ACTIVATED) {
+      ROS_INFO_STREAM("CHEAT CODE ACTIVATED, SWITCHING TO AUTO MODE");
+      followingMode_ = AUTO;
+    }
+
+  } else if (signal_monitors[deadman_monitor].get_desired_action() == CONTINUE) {
+
+    if (followingMode_ == AUTO) {
+
+      // Check if we need to disable AUTO mode
+      for (int idx = 0; idx < msg->buttons.size(); ++idx) {
+        ros::Duration diff = ros::Time::now() - timeofLastCodeSequence_;
+
+        if (msg->buttons[idx] == 1 && diff.toSec() > 1.0) {
+          ROS_INFO("CHEAT CODE DEACTIVATED, SWITCHING TO MANUAL MODE\n");
+          followingMode_ = MANUAL;
+          codeState_ = 0;
         }
+      }
 
-        // check the cheat code state
-        checkCodeState(msg);
-        // if the cheat code has been activated, then put the path tracker into auto mode.
-        if(codeState_ == CHEAT_CODE_ACTIVATED){
-            ROS_INFO_STREAM("CHEAT CODE ACTIVATED, SWITCHING TO AUTO MODE");
-            followingMode_ =AUTO;
-        }
+      // Now that we've checked the cheat code and maybe changed followingMode_...
+      if (followingMode_ == MANUAL && pressed == false) {
+        signal_monitors[deadman_monitor].set_monitor_desired_action(PAUSE);
 
-    } else if (signal_monitors[deadman_monitor].get_desired_action() == CONTINUE){
+      } else {
+        signal_monitors[deadman_monitor].set_monitor_desired_action(CONTINUE);
+      }
 
-        if (followingMode_ == AUTO){
-
-            // Check if we need to disable AUTO mode
-            for(int idx = 0; idx < msg->buttons.size(); ++idx){
-                ros::Duration diff = ros::Time::now()-timeofLastCodeSequence_;
-
-                if(msg->buttons[idx] == 1 && diff.toSec() > 1.0){
-                    ROS_INFO("CHEAT CODE DEACTIVATED, SWITCHING TO MANUAL MODE\n");
-                    followingMode_ = MANUAL;
-                    codeState_ = 0;
-                }
-            }
-
-            // Now that we've checked the cheat code and maybe changed followingMode_...
-            if (followingMode_ == MANUAL && pressed == false){
-                signal_monitors[deadman_monitor].set_monitor_desired_action(PAUSE);
-
-            } else {
-                signal_monitors[deadman_monitor].set_monitor_desired_action(CONTINUE);
-            }
-
-        } else if (pressed == false){
-            signal_monitors[deadman_monitor].set_monitor_desired_action(PAUSE);
-
-        } else {
-            signal_monitors[deadman_monitor].register_msg_but_no_status_change();
-
-        }
+    } else if (pressed == false) {
+      signal_monitors[deadman_monitor].set_monitor_desired_action(PAUSE);
 
     } else {
-        signal_monitors[deadman_monitor].set_monitor_desired_action(PAUSE);
+      signal_monitors[deadman_monitor].register_msg_but_no_status_change();
+
     }
+
+  } else {
+    signal_monitors[deadman_monitor].set_monitor_desired_action(PAUSE);
+  }
 
 }
 
-void asrl::safetyMonitor::deadman_monitor_input::checkCodeState(const sensor_msgs::Joy::Ptr & msg)
-{
-	ros::Duration timeDiff = ros::Time::now() - timeofLastCodeSequence_;
-	//ROS_INFO("Time since last button: %f \n",timeDiff.toSec());
+void DeadmanMonitorInput::checkCodeState(const sensor_msgs::msg::Joy::Ptr &msg) {
+  ros::Duration timeDiff = ros::Time::now() - timeofLastCodeSequence_;
+  //ROS_INFO("Time since last button: %f \n",timeDiff.toSec());
 
-	if(timeDiff.toSec() > 0.75){
-		codeState_ = 0;
-        //ROS_INFO_STREAM("Too long since button press, codeState_ reset.");
+  if (timeDiff.toSec() > 0.75) {
+    codeState_ = 0;
+    //ROS_INFO_STREAM("Too long since button press, codeState_ reset.");
 
+  }
+  if (codeState_ < 16) {
+
+    if (msg->axes[axis_array[codeState_]] == value_array[codeState_]) {
+      codeState_++;
+      timeofLastCodeSequence_ = ros::Time::now();
     }
-	if (codeState_ < 16){
 
-        if (msg->axes[axis_array[codeState_]] == value_array[codeState_]){
-            codeState_++;
-            timeofLastCodeSequence_ = ros::Time::now();
-        }
-
-    } else if (codeState_ >= 16 && msg->buttons[deadman_button_index_] != 0){
-        codeState_ = CHEAT_CODE_ACTIVATED;
-	} else if (codeState_ == CHEAT_CODE_ACTIVATED){
-        // Good shit.
-	} else {
-        // fail.
-	}
+  } else if (codeState_ >= 16 && msg->buttons[deadman_button_index_] != 0) {
+    codeState_ = CHEAT_CODE_ACTIVATED;
+  } else if (codeState_ == CHEAT_CODE_ACTIVATED) {
+    // Good shit.
+  } else {
+    // fail.
+  }
 }
 
-#endif
+} // safety_monitor
+} // vtr
