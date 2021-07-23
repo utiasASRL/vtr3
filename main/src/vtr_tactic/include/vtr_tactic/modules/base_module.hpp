75c4dd7e6b6c56b96b1d0124c0f662f43e96d9ee
Yuchen Wu <cheneyuwu@gmail.com>
2021-07-23 17:19:21 -0400

    debug logs

--- main/src/vtr_tactic/include/vtr_tactic/modules/base_module.hpp
+++ main/src/vtr_tactic/include/vtr_tactic/modules/base_module.hpp
@@ -31,22 +31,25 @@ class BaseModule {
    * \brief Runs the module with necessary timing or other type of monitoring.
    */
   void initialize(MapCache &mdata, const Graph::ConstPtr &graph) {
-    LOG(DEBUG) << "\033[1;31mInitializing module: " << getName() << "\033[0m";
+    CLOG(DEBUG, "tactic.module")
+        << "\033[1;31mInitializing module: " << getName() << "\033[0m";
     timer.reset();
     initializeImpl(mdata, graph);
-    LOG(DEBUG) << "Finished initializing module: " << getName()
-               << ", which takes " << timer;
+    CLOG(DEBUG, "tactic.module")
+        << "Finished initializing module: " << getName() << ", which takes "
+        << timer;
   }
 
   /**
    * \brief Runs the module with necessary timing or other type of monitoring.
    */
   void run(QueryCache &qdata, MapCache &mdata, const Graph::ConstPtr &graph) {
-    LOG(DEBUG) << "\033[1;31mRunning module: " << getName() << "\033[0m";
+    CLOG(DEBUG, "tactic.module")
+        << "\033[1;31mRunning module: " << getName() << "\033[0m";
     timer.reset();
     runImpl(qdata, mdata, graph);
-    LOG(DEBUG) << "Finished running module: " << getName() << ", which takes "
-               << timer;
+    CLOG(DEBUG, "tactic.module") << "Finished running module: " << getName()
+                                 << ", which takes " << timer;
   }
 
   /**
@@ -56,21 +59,24 @@ class BaseModule {
    */
   void updateGraph(QueryCache &qdata, MapCache &mdata, const Graph::Ptr &graph,
                    VertexId live_id) {
-    LOG(DEBUG) << "\033[1;32mUpdating graph module: " << getName() << "\033[0m";
+    CLOG(DEBUG, "tactic.module")
+        << "\033[1;32mUpdating graph module: " << getName() << "\033[0m";
     timer.reset();
     updateGraphImpl(qdata, mdata, graph, live_id);
-    LOG(DEBUG) << "Finished updating graph module: " << getName()
-               << ", which takes " << timer;
+    CLOG(DEBUG, "tactic.module")
+        << "Finished updating graph module: " << getName() << ", which takes "
+        << timer;
   }
 
   /** \brief Visualize data in this module. */
   void visualize(QueryCache &qdata, MapCache &mdata,
                  const Graph::ConstPtr &graph) {
-    LOG(DEBUG) << "\033[1;33mVisualizing module: " << getName() << "\033[0m";
+    CLOG(DEBUG, "tactic.module")
+        << "\033[1;33mVisualizing module: " << getName() << "\033[0m";
     timer.reset();
     visualizeImpl(qdata, mdata, graph, vis_mtx_);
-    LOG(DEBUG) << "Finished visualizing module: " << getName()
-               << ", which takes " << timer;
+    CLOG(DEBUG, "tactic.module") << "Finished visualizing module: " << getName()
+                                 << ", which takes " << timer;
   }
 
   virtual void configFromROS(const rclcpp::Node::SharedPtr &,
