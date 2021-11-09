a97102a7992f15aaa44aeda2ebefd4a637161e81
Yuchen Wu <cheneyuwu@gmail.com>
2021-11-09 00:03:17 -0500

    honeycomb script

--- extra/src/vtr_testing_honeycomb/script/commands.sh
+++ extra/src/vtr_testing_honeycomb/script/commands.sh
@@ -6,26 +6,27 @@ export VTHRESULT=${VTRTEMP}/testing/lidar/honeycomb.exp
 mkdir -p ${VTHRESULT}
 
 ## Perform odometry on a sequence directly
-ODO_INPUT=rosbag2_2021_08_23-09_25_37
+DATASET=${VTHDATA}/utias_20211101_parkinglot_shorter_sequence
+ODO_INPUT=rosbag2_2021_11_06-11_58_53
 #        package            executable                                      namespace      parameter file                          data directory (output dir)       input directory
 ros2 run vtr_testing_honeycomb vtr_testing_honeycomb_odometry_direct  \
   --ros-args  -r __ns:=/vtr  --params-file ${VTHCONFIG}/honeycomb.yaml \
   -p data_dir:=${VTHRESULT}/${ODO_INPUT}/${ODO_INPUT}/honeycomb \
-  -p odo_dir:=${VTHDATA}/${ODO_INPUT}
+  -p odo_dir:=${DATASET}/${ODO_INPUT}
 
 ## Perform localization on a sequence directly (with a specified point map version)
-ODO_INPUT=rosbag2_2021_08_23-09_25_37
-LOC_INPUT=rosbag2_2021_08_23-09_57_54
+ODO_INPUT=rosbag2_2021_11_06-11_58_53
+LOC_INPUT=rosbag2_2021_08_23-11_58_52
 ros2 run vtr_testing_honeycomb vtr_testing_honeycomb_localization_direct \
   --ros-args  -r __ns:=/vtr  --params-file ${VTHCONFIG}/honeycomb.yaml \
   -p localization.recall.map_version:=point_map_v0 \
   -p data_dir:=${VTHRESULT}/${ODO_INPUT}/${LOC_INPUT}/honeycomb \
-  -p odo_dir:=${VTHDATA}/${ODO_INPUT} \
-  -p loc_dir:=${VTHDATA}/${LOC_INPUT}
+  -p odo_dir:=${DATASET}/${ODO_INPUT} \
+  -p loc_dir:=${DATASET}/${LOC_INPUT}
 
 # ## Perform offline tasks
-# ODO_INPUT=rosbag2_2021_08_23-09_25_37
-# LOC_INPUT=rosbag2_2021_08_23-09_57_54
+# ODO_INPUT=rosbag2_2021_11_06-11_58_53
+# LOC_INPUT=rosbag2_2021_08_23-11_58_52
 # ros2 run vtr_testing_honeycomb vtr_testing_honeycomb_intra_exp_merging \
 #   --ros-args  -r __ns:=/vtr  --params-file ${VTHCONFIG}/honeycomb.yaml \
 #   -p data_dir:=${VTHRESULT}/${ODO_INPUT}/${ODO_INPUT}/honeycomb
--- extra/src/vtr_testing_honeycomb/script/obelisk.sh
+++ extra/src/vtr_testing_honeycomb/script/obelisk.sh
@@ -3,16 +3,31 @@
 ## Define the following environment variables VTH=VTR Testing honeycomb
 export VTHROOT=${VTRSRC}/extra/src/vtr_testing_honeycomb
 export VTHCONFIG=${VTHROOT}/config
-export VTHDATA=${VTRDATA}/utias_20211101_parkinglot_shorter_sequence
+export VTHDATA=${VTRDATA}
 export VTHRESULT=${VTRTEMP}/testing/lidar/honeycomb.exp
 mkdir -p ${VTHRESULT}
 
-
-ODO_INPUT='rosbag2_2021_11_01-18_05_58'
+DATASET=${VTHDATA}/utias_20211101_parkinglot_shorter_sequence
+ODO_INPUT=rosbag2_2021_11_06-19_57_56
 LOC_INPUTS=(
-  'rosbag2_2021_11_01-18_10_03'
-  'rosbag2_2021_11_01-18_14_04'
-  'rosbag2_2021_11_01-18_18_34'
+  ## parking lot sequence
+  rosbag2_2021_11_01-18_05_58
+  rosbag2_2021_11_01-18_10_03
+  rosbag2_2021_11_01-18_14_04
+  rosbag2_2021_11_01-18_18_34
+  rosbag2_2021_11_06-11_41_33
+  rosbag2_2021_11_06-11_45_39
+  rosbag2_2021_11_06-11_58_53
+  rosbag2_2021_11_06-12_07_03
+  # rosbag2_2021_11_06-19_57_56
+  rosbag2_2021_11_06-20_05_35
+  ## short straight line
+  # rosbag2_2021_11_06-20_13_42
+  # rosbag2_2021_11_06-20_29_52
+  # rosbag2_2021_11_06-20_34_11
+  # rosbag2_2021_11_06-20_38_08
+  # rosbag2_2021_11_06-20_41_24
+  # rosbag2_2021_11_06-20_48_00  # This is the one without boreas car
 )
 
 ## source
@@ -23,11 +38,11 @@ source ${VTRSRC}/extra/install/setup.bash
 echo "[COMMAND] creating odometry data directory at ${VTHRESULT}/${ODO_INPUT}/${ODO_INPUT}/honeycomb"
 mkdir -p ${VTHRESULT}/${ODO_INPUT}/${ODO_INPUT}
 
-echo "[COMMAND] running odometry with input ${VTHDATA}/${ODO_INPUT}"
+echo "[COMMAND] running odometry with input ${DATASET}/${ODO_INPUT}"
 ros2 run vtr_testing_honeycomb vtr_testing_honeycomb_odometry_direct  \
   --ros-args  -r __ns:=/vtr  --params-file ${VTHCONFIG}/honeycomb.yaml \
   -p data_dir:=${VTHRESULT}/${ODO_INPUT}/${ODO_INPUT}/honeycomb \
-  -p odo_dir:=${VTHDATA}/${ODO_INPUT}
+  -p odo_dir:=${DATASET}/${ODO_INPUT}
 
 # backup odometry results
 cp -r ${VTHRESULT}/${ODO_INPUT}/${ODO_INPUT}/honeycomb \
@@ -40,10 +55,10 @@ for LOC_INPUT in "${LOC_INPUTS[@]}"; do
   cp -r ${VTHRESULT}/${ODO_INPUT}/${ODO_INPUT}/honeycomb \
     ${VTHRESULT}/${ODO_INPUT}/${LOC_INPUT}
 
-  echo "[COMMAND] running odometry with input ${VTHDATA}/${LOC_INPUT}"
+  echo "[COMMAND] running odometry with input ${DATASET}/${LOC_INPUT}"
   ros2 run vtr_testing_honeycomb vtr_testing_honeycomb_localization_direct \
   --ros-args  -r __ns:=/vtr  --params-file ${VTHCONFIG}/honeycomb.yaml \
   -p data_dir:=${VTHRESULT}/${ODO_INPUT}/${LOC_INPUT}/honeycomb \
-  -p odo_dir:=${VTHDATA}/${ODO_INPUT} \
-  -p loc_dir:=${VTHDATA}/${LOC_INPUT}
+  -p odo_dir:=${DATASET}/${ODO_INPUT} \
+  -p loc_dir:=${DATASET}/${LOC_INPUT}
 done
