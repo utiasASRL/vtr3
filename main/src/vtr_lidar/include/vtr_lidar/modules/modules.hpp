05dc487966d28e38ae82101800d137c14bbaddda
Jordy Sehn <jordy.sehn@robotics.utias.utoronto.ca>
2023-01-24 11:45:35 -0500

    Added new module for ouster lidar

--- main/src/vtr_lidar/include/vtr_lidar/modules/modules.hpp
+++ main/src/vtr_lidar/include/vtr_lidar/modules/modules.hpp
@@ -23,6 +23,7 @@
 #include "vtr_lidar/modules/preprocessing/conversions/honeycomb_conversion_module_v2.hpp"
 #include "vtr_lidar/modules/preprocessing/conversions/velodyne_conversion_module.hpp"
 #include "vtr_lidar/modules/preprocessing/conversions/velodyne_conversion_module_v2.hpp"
+#include "vtr_lidar/modules/preprocessing/conversions/ouster_conversion_module.hpp"
 #include "vtr_lidar/modules/preprocessing/preprocessing_module.hpp"
 #include "vtr_lidar/modules/preprocessing/preprocessing_module_v2.hpp"
 
