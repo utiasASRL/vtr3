2d1241db38ec722c48eedc2648f356a895bd7d2b
Daniil <lisus.daniil@gmail.com>
2025-01-24 11:07:56 -0500

    Updated radar_lidar pipeline to work with new radar pipeline

--- main/src/vtr_radar_lidar/include/vtr_radar_lidar/pipeline.hpp
+++ main/src/vtr_radar_lidar/include/vtr_radar_lidar/pipeline.hpp
@@ -47,6 +47,7 @@ class RadarLidarPipeline : public tactic::BasePipeline {
     double submap_rotation_threshold = 0.0;     // in degrees
 
     bool save_raw_point_cloud = false;
+    bool save_radar_images = false;
 
     static ConstPtr fromROS(const rclcpp::Node::SharedPtr &node,
                             const std::string &param_prefix);
@@ -96,9 +97,12 @@ class RadarLidarPipeline : public tactic::BasePipeline {
   std::shared_ptr<radar::PointMap<radar::PointWithInfo>> sliding_map_odo_;
   /** \brief current timestamp*/
   std::shared_ptr<tactic::Timestamp> timestamp_odo_;
+  std::shared_ptr<tactic::Timestamp> timestamp_odo_radar_;
   /** \brief current pose and body velocity w.r.t the sliding map */
   std::shared_ptr<tactic::EdgeTransform> T_r_m_odo_;
+  std::shared_ptr<tactic::EdgeTransform> T_r_m_odo_radar_;
   std::shared_ptr<Eigen::Matrix<double, 6, 1>> w_m_r_in_r_odo_;
+  std::shared_ptr<Eigen::Matrix<double, 6, 1>> w_m_r_in_r_odo_radar_;
   /** \brief vertex id of the last submap */
   tactic::VertexId submap_vid_odo_ = tactic::VertexId::Invalid();
   /** \brief transformation from latest submap vertex to robot */
@@ -107,6 +111,12 @@ class RadarLidarPipeline : public tactic::BasePipeline {
   /// localization cached data
   /** \brief Current submap for localization */
   std::shared_ptr<const lidar::PointMap<lidar::PointWithInfo>> submap_loc_;
+
+  /// preintegration cached data
+  std::shared_ptr<tactic::Timestamp> preint_start_time_;
+  std::shared_ptr<tactic::Timestamp> preint_end_time_;
+  std::shared_ptr<sensor_msgs::msg::Imu> last_gyro_msg_;
+  std::shared_ptr<float> preint_delta_yaw_;
 
   VTR_REGISTER_PIPELINE_DEC_TYPE(RadarLidarPipeline);
 };
