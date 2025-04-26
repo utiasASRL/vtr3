67043bccfa9e9a031646881bb47970f210680b86
a-krawciw <freakyfactoid@gmail.com>
2025-04-25 23:42:51 -0400

    Add more debug info.

--- main/src/vtr_path_planning/scripts/export_unicycle.py
+++ main/src/vtr_path_planning/scripts/export_unicycle.py
@@ -7,14 +7,19 @@ import os
 
 solver.generate_dependencies("mpc_solver.cpp", {"cpp": True, "with_header": True})
 print("Generated MPC files.", file=sys.stderr)
-if os.path.exists('mpc_solver.cpp'):
-    shutil.move('mpc_solver.cpp', os.getenv("VTRSRC") + '/main/src/vtr_path_planning/src/mpc/mpc_solver.cpp')
-elif not os.path.exists(os.getenv("VTRSRC") + '/main/src/vtr_path_planning/src/mpc/mpc_solver.cpp'):
-    raise FileNotFoundError("An error occurred while exporting the MPC.")
+try:
+    if os.path.exists('mpc_solver.cpp'):
+        shutil.move('mpc_solver.cpp', os.getenv("VTRSRC") + '/main/src/vtr_path_planning/src/mpc/mpc_solver.cpp')
+    elif not os.path.exists(os.getenv("VTRSRC") + '/main/src/vtr_path_planning/src/mpc/mpc_solver.cpp'):
+        raise FileNotFoundError("An error occurred while exporting the MPC.")
 
-if os.path.exists('mpc_solver.h'):
-    shutil.move('mpc_solver.h', os.getenv("VTRSRC") + '/main/src/vtr_path_planning/include/vtr_path_planning/mpc/mpc_solver.hpp')
-elif not os.path.exists(os.getenv("VTRSRC") + '/main/src/vtr_path_planning/include/vtr_path_planning/mpc/mpc_solver.hpp'):
-    raise FileNotFoundError("An error occurred while exporting the MPC.")
 
-print("Copied files successfully", file=sys.stderr)
+    if os.path.exists('mpc_solver.h'):
+        shutil.move('mpc_solver.h', os.getenv("VTRSRC") + '/main/src/vtr_path_planning/include/vtr_path_planning/mpc/mpc_solver.hpp')
+    elif not os.path.exists(os.getenv("VTRSRC") + '/main/src/vtr_path_planning/include/vtr_path_planning/mpc/mpc_solver.hpp'):
+        raise FileNotFoundError("An error occurred while exporting the MPC.")
+
+    print("Copied files successfully", file=sys.stderr)
+except Exception as e:
+    print(f"Unknown error. Debug info pwd: {os.getcwd()}\nls: {os.listdir(os.getcwd())}", file=sys.stderr)
+    raise e
