845ba1d13bf756d9ce7bd55e11b59af9f0ae38f5
a-krawciw <freakyfactoid@gmail.com>
2025-05-28 22:44:10 +0000

    Updates to include proper jetson support.

--- main/src/vtr_navigation/launch/vtr.launch.py
+++ main/src/vtr_navigation/launch/vtr.launch.py
@@ -19,6 +19,7 @@ def generate_launch_description():
         "namespace": 'vtr',
         "executable": 'vtr_navigation',
         "output": 'screen',
+        "remappings": [("/vtr/command", "/cmd_vel")],
         #"prefix": 'xterm -e gdb -ex run --args',
         #"prefix": 'valgrind --tool=callgrind',
     }
