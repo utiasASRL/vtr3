ad484c22f16f196f205985aadbde459b08054296
lanton97 <l.antonyshyn@hotmail.com>
2025-05-30 15:51:13 -0400

    Reduce duplication in MPC export file

--- main/src/vtr_path_planning/scripts/export_mpc.py
+++ main/src/vtr_path_planning/scripts/export_mpc.py
@@ -6,14 +6,22 @@ from bicycle_solver_rw import solver as bi_solver
 import shutil
 import os
 
-uni_solver.generate_dependencies("unicycle_mpc_solver.cpp", {"cpp": True, "with_header": True})
-shutil.move('unicycle_mpc_solver.cpp', os.getenv("VTRSRC") + '/main/src/vtr_path_planning/src/mpc/unicycle_mpc_solver.cpp')
-shutil.move('unicycle_mpc_solver.h', os.getenv("VTRSRC") + '/main/src/vtr_path_planning/include/vtr_path_planning/mpc/unicycle_mpc_solver.hpp')
+def gen_mv_mpc_cpp(controller, controller_name:str, src_path:str, inc_path:str) -> None:
+    controller.generate_dependencies(controller_name + '_mpc_solver.cpp', {'cpp': True, 'with_header': True})
+    shutil.move(controller_name + '_mpc_solver.cpp', src_path + controller_name + '_mpc_solver.cpp')
+    shutil.move(controller_name + '_mpc_solver.h', inc_path + controller_name + '_mpc_solver.hpp')
 
-acker_solver.generate_dependencies("ackermann_mpc_solver.cpp", {"cpp": True, "with_header": True})
-shutil.move('ackermann_mpc_solver.cpp', os.getenv("VTRSRC") + '/main/src/vtr_path_planning/src/mpc/ackermann_mpc_solver.cpp')
-shutil.move('ackermann_mpc_solver.h', os.getenv("VTRSRC") + '/main/src/vtr_path_planning/include/vtr_path_planning/mpc/ackermann_mpc_solver.hpp')
 
-bi_solver.generate_dependencies("bicycle_mpc_solver.cpp", {"cpp": True, "with_header": True})
-shutil.move('bicycle_mpc_solver.cpp', os.getenv("VTRSRC") + '/main/src/vtr_path_planning/src/mpc/bicycle_mpc_solver.cpp')
-shutil.move('bicycle_mpc_solver.h', os.getenv("VTRSRC") + '/main/src/vtr_path_planning/include/vtr_path_planning/mpc/bicycle_mpc_solver.hpp')
+src_path = os.path.join(os.getenv("VTRSRC"), 'main/src/vtr_path_planning/src/mpc/')
+if not os.path.exists(src_path):
+    os.mkdir(src_path)
+
+inc_path = os.path.join(os.getenv("VTRSRC"), 'main/src/vtr_path_planning/include/vtr_path_planning/mpc/')
+if not os.path.exists(inc_path):
+    os.mkdir(inc_path)
+
+
+gen_mv_mpc_cpp(uni_solver, 'unicycle', src_path, inc_path)
+gen_mv_mpc_cpp(acker_solver, 'ackermann', src_path, inc_path)
+gen_mv_mpc_cpp(bi_solver, 'bicycle', src_path, inc_path)
+
