a0673e47e3dde780f451657d4034ef8ab2f4f941
Sven Lilge <sven.lilge@utoronto.ca>
2025-08-11 14:26:57 -0400

    Added arclength waypoint method and slight change to euclidean method

--- main/src/vtr_path_planning/src/mpc/follower_common.cpp
+++ main/src/vtr_path_planning/src/mpc/follower_common.cpp
@@ -91,7 +91,10 @@ PoseResultHomotopy generateFollowerReferencePosesEuclidean(const TransformList& 
   std::vector<int> leader_pose_done(leader_world_poses.size(), 0);
   std::vector<lgmath::se3::Transformation> best_pose(leader_world_poses.size());
 
-  for(double p = robot_p; p < final_leader_p_value; p += 0.02) {
+
+  double p_start = robot_p - 1.0;
+
+  for(double p = p_start; p < final_leader_p_value; p += 0.02) {
     tactic::SegmentInfo closestSegment = findClosestSegment(p, chain, chain->trunkSequenceId());
     double interp = std::clamp((p - chain->p(closestSegment.start_sid)) / (chain->p(closestSegment.end_sid) - chain->p(closestSegment.start_sid)), 0.0, 1.0);
     lgmath::se3::Transformation pose = interpolatePoses(interp, chain->pose(closestSegment.start_sid), chain->pose(closestSegment.end_sid));
@@ -120,6 +123,100 @@ PoseResultHomotopy generateFollowerReferencePosesEuclidean(const TransformList& 
 
     }
   }
+  follower_reference.poses = best_pose;
+  for (const auto& width : best_width){
+    follower_reference.barrier_q_min.push_back(-width);
+    follower_reference.barrier_q_max.push_back(width);
+  }
+
+  return follower_reference;
+}
+
+
+
+PoseResultHomotopy generateFollowerReferencePosesArclength(const TransformList& leader_world_poses, const double final_leader_p_value, const tactic::LocalizationChain::Ptr chain, double robot_p, double target_distance) {
+  PoseResultHomotopy follower_reference;
+  
+  // Run through the path and find the pose that best fulfills the distance constraint
+  std::vector<double> best_distance(leader_world_poses.size(), std::numeric_limits<double>::max());
+  std::vector<double> leader_arclength(leader_world_poses.size());
+  std::vector<double> best_width(leader_world_poses.size(), std::numeric_limits<double>::max());
+  std::vector<lgmath::se3::Transformation> best_pose(leader_world_poses.size());
+
+  std::map<double, lgmath::se3::Transformation> arclength_map;
+  std::vector<tactic::SegmentInfo> segments;
+  std::vector<double> interp_value;
+  lgmath::se3::Transformation last_pose;
+  double cur_arclength = 0.0;
+
+  double p_start = robot_p - 1.0;
+
+  for(double p = p_start; p < final_leader_p_value; p += 0.02) {
+    tactic::SegmentInfo closestSegment = findClosestSegment(p, chain, chain->trunkSequenceId());
+    double interp = std::clamp((p - chain->p(closestSegment.start_sid)) / (chain->p(closestSegment.end_sid) - chain->p(closestSegment.start_sid)), 0.0, 1.0);
+    lgmath::se3::Transformation pose = interpolatePoses(interp, chain->pose(closestSegment.start_sid), chain->pose(closestSegment.end_sid));
+
+    if(p == p_start) 
+    {
+      last_pose = pose;
+      arclength_map[0] = pose;
+    }
+    else{
+      cur_arclength = cur_arclength + (last_pose.inverse()*pose).r_ab_inb().norm();
+      last_pose = pose;
+      arclength_map[cur_arclength] = pose;
+    }
+    segments.push_back(closestSegment);
+    interp_value.push_back(interp);
+
+    // Check if the current pose corresponds to any of the leader poses
+    for (uint i = 0; i < leader_world_poses.size(); i++){
+      // Leader pose in world frame
+      auto T_w_l = leader_world_poses[i];
+      double dist = (pose.inverse() * T_w_l).r_ab_inb().norm();
+      if (dist < best_distance[i]) {
+        best_distance[i] = dist;
+        leader_arclength[i] = cur_arclength;
+      }
+    }
+  }
+
+  //Run through the leader arclengths we just found
+  for(uint i = 0; i < leader_arclength.size(); i++){
+    double follower_arclength = leader_arclength[i] - target_distance;
+
+    int seg_idx = 0;
+
+    // Find the closest arclength in the map
+    auto it = arclength_map.lower_bound(follower_arclength);
+    if (it == arclength_map.end()) {
+      // If follower_arclength is beyond the last key, use the last pose
+      it--;
+      best_pose[i] = it->second;
+      seg_idx = segments.size() - 1;
+    } else if (it == arclength_map.begin()) {
+      // If follower_arclength is before the first key, use the first pose
+      best_pose[i] = it->second;
+      seg_idx = 0;
+    } else {
+      // Pick the closest pose (either it or it_prev)
+      auto it_prev = std::prev(it);
+      if (std::abs(follower_arclength - it_prev->first) < std::abs(follower_arclength - it->first)) {
+        best_pose[i] = it_prev->second;
+        seg_idx = std::distance(arclength_map.begin(), it_prev);
+      } else {
+        best_pose[i] = it->second;
+        seg_idx = std::distance(arclength_map.begin(), it);
+      }
+    }
+    
+    auto width1 = pose_graph::BasicPathBase::terrian_type_corridor_width(chain->query_terrain_type(segments[seg_idx].start_sid));
+    auto width2 = pose_graph::BasicPathBase::terrian_type_corridor_width(chain->query_terrain_type(segments[seg_idx].end_sid));
+    double interp_width = (1 - interp_value[seg_idx]) * width1 + interp_value[seg_idx] * width2;
+    best_width[i] = interp_width;
+
+  }
+
   follower_reference.poses = best_pose;
   for (const auto& width : best_width){
     follower_reference.barrier_q_min.push_back(-width);
