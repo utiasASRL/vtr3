8776f3e5b462e7d5d7a0a8ed124af5f049fc6fb3
lanton97 <l.antonyshyn@hotmail.com>
2025-08-31 23:43:30 +0000

    Initial commit for convoy manager node

---
+++ main/src/vtr_multi_robot/launch/convoy_manager.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+import os
+import os.path as osp
+
+def generate_launch_description():
+    package_name = 'vtr_multi_robot'
+    config_dir = osp.join(os.getenv('VTRSRC', ''), 'config')
+    return LaunchDescription([
+        DeclareLaunchArgument('base_params', description='base parameter file (sensor, robot specific)'),
+        DeclareLaunchArgument('robots', default_value='[\"robot1\",\"robot2\"]', description='list of robot names'),
+        Node(
+            package='vtr_multi_robot',
+            executable='convoy_manager',
+            name='convoy_manager',
+            output='screen',
+            parameters=[
+                PathJoinSubstitution([config_dir, LaunchConfiguration('base_params')]),
+                {'robots': LaunchConfiguration('robots')}
+            ]
+        ),
+    ])
