e60a42a9470261e340d692e836820587fe6ef8b6
Katya Papais <katyapapais@outlook.com>
2025-10-22 16:52:03 -0400

    fixed weird merge artifacts

--- main/src/vtr_navigation/src/navigator.cpp
+++ main/src/vtr_navigation/src/navigator.cpp
@@ -310,9 +310,12 @@ if (pipeline->name() == "radar") {
 
   // ----- compute gyro bias
   // gyro_bias_ = loadGyroBias(gyro_topic);
-  // gyro_bias_ << -0.0115121, -0.00701742, -0.00503122;   // temp hardcoded value for offline testing
-  gyro_bias_ << -0.0112692, -0.00706693, -0.00526226;   //  loop3 large
-  // gyro_bias_ <<  -0.0112731, -0.00725823, -0.00593503; // fig 8 run1
+  // gyro_bias_ << -0.0115121, -0.00701742, -0.00503122;     // temp hardcoded value for offline testing
+  // gyro_bias_ << -0.0112692, -0.00706693, -0.00526226;     // loop3 large
+  // gyro_bias_ << -0.0112731, -0.00725823, -0.00593503;     // fig 8 run1
+  // gyro_bias_ << -0.0117018, -0.00694546, -0.00463976;     // may05 teach47
+  // gyro_bias_ << -0.0103390, -0.00727331,  -0.0054914;     // 1021 feature
+  gyro_bias_ << -0.0102267, -0.00724319, -0.00547418;     // 1021 featureless
   CLOG(INFO, "navigation") << "Gyro bias loaded: " << gyro_bias_.transpose();
 
   std::cout << "############################ gyro bias : " << gyro_bias_.transpose() << "############################" << std::endl;
