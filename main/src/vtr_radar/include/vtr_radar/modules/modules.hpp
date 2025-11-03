509a4e6ac3952d814df71e34c22809c0a9b7a2ae
Daniil <lisus.daniil@gmail.com>
2025-11-03 17:05:48 -0500

    Removing non-functioning Doppler odometry module to merge into clean main branch

--- main/src/vtr_radar/include/vtr_radar/modules/modules.hpp
+++ main/src/vtr_radar/include/vtr_radar/modules/modules.hpp
@@ -25,7 +25,6 @@
 #include "vtr_radar/modules/preprocessing/preprocessing_module.hpp"
 
 #include "vtr_radar/modules/odometry/odometry_gyro_module.hpp"
-#include "vtr_radar/modules/odometry/odometry_doppler_module.hpp"
 
 #include "vtr_radar/modules/odometry/odometry_icp_module.hpp"
 #include "vtr_radar/modules/odometry/odometry_se3_icp_module.hpp"
