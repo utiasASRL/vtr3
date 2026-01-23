9a7aa2f8ed13b8d66f32fc725b76ae5ada25647b
lanton97 <l.antonyshyn@hotmail.com>
2026-01-23 10:09:26 -0500

    Point unicycle at base MPC class - no corresponding changes casadi SO yet

--- main/src/vtr_path_planning/include/vtr_path_planning/mpc/unicycle_mpc_path_tracker.hpp
+++ main/src/vtr_path_planning/include/vtr_path_planning/mpc/unicycle_mpc_path_tracker.hpp
@@ -20,7 +20,7 @@
 
 #include <vtr_logging/logging.hpp>
 #include <vtr_tactic/types.hpp>
-#include <vtr_path_planning/base_path_planner.hpp>
+#include <vtr_path_planning/mpc/base_mpc_path_tracker.hpp>
 #include <vtr_path_planning/mpc/casadi_path_planners.hpp>
 #include <vtr_path_planning/mpc/speed_scheduler.hpp>
 
@@ -29,38 +29,28 @@
 namespace vtr {
 namespace path_planning {
 
-class UnicycleMPCPathTracker : public BasePathPlanner {
+class UnicycleMPCPathTracker : public BaseMPCPathTracker {
  public:
   PTR_TYPEDEFS(UnicycleMPCPathTracker);
 
   static constexpr auto static_name = "unicycle_mpc";
 
   // Note all rosparams that are in the config yaml file need to be declared here first, though they can be then changes using the declareparam function for ros in the cpp file
-  struct Config : public BasePathPlanner::Config {
+  struct Config : public BaseMPCPathTracker::Config {
     PTR_TYPEDEFS(Config);
 
-    // Speed Scheduler
-    double planar_curv_weight = 2.50;
-    double profile_curv_weight = 0.5; 
-    double eop_weight = 1.0;
-    double min_vel = 0.5;  
-
-    // MPC Configs
-    bool extrapolate_robot_pose = true;
-    bool mpc_verbosity = false;
-    double forward_vel = 0.75;
-    double max_lin_vel = 1.25;
-    double max_ang_vel = 0.75;
-    double max_lin_acc = 10.0;
-    double max_ang_acc = 10.0;
-    double robot_linear_velocity_scale = 1.0;
-    double robot_angular_velocity_scale = 1.0;
-
     // Add unicycle model param
+    double q_x = 0.0;
+    double q_y = 0.0;
+    double q_th = 0.0;
+    double r1 = 0.0;
+    double r2 = 0.0;
+    double racc1 = 0.0;
+    double racc2 = 0.0;
 
-    // Misc
-    int command_history_length = 100;
-
+    static void loadConfig(Config::Ptr config,  
+		           const rclcpp::Node::SharedPtr& node,
+                           const std::string& prefix = "path_planning");
 
     static Ptr fromROS(const rclcpp::Node::SharedPtr& node,
                        const std::string& prefix = "path_planning");
@@ -73,24 +63,26 @@ class UnicycleMPCPathTracker : public BasePathPlanner {
   ~UnicycleMPCPathTracker() override;
 
  protected:
-  void initializeRoute(RobotState& robot_state);
-  Command computeCommand(RobotState& robot_state) override;
+ 
+  void loadMPCConfig(
+      CasadiUnicycleMPC::Config::Ptr mpc_config, Eigen::Matrix<double, 6, 1> w_p_r_in_r, Eigen::Vector2d applied_vel);
+  virtual CasadiMPC::Config::Ptr getMPCConfig(Eigen::Matrix<double, 6, 1> w_p_r_in_r, Eigen::Vector2d applied_vel, const bool) override;
+
+  virtual bool isMPCStateValid(CasadiMPC::Config::Ptr mpcConfig, const tactic::Timestamp& curr_time) override;
+  
+  void loadMPCPath(CasadiMPC::Config::Ptr mpcConfig, const lgmath::se3::Transformation& T_w_p,
+                            const lgmath::se3::Transformation& T_p_r_extp,
+                            const double state_p,
+                            RobotState& robot_state, 
+                            const tactic::Timestamp& curr_time) override;
+
+  virtual std::map<std::string, casadi::DM> callSolver(CasadiMPC::Config::Ptr config) override;
 
  private: 
   VTR_REGISTER_PATH_PLANNER_DEC_TYPE(UnicycleMPCPathTracker);
 
   Config::ConstPtr config_;
   CasadiUnicycleMPC solver_;
-
-  // Store the previously applied velocity and a sliding window history of MPC results
-  Eigen::Vector2d applied_vel_;
-  std::vector<Eigen::Vector2d> vel_history;
-  tactic::Timestamp prev_vel_stamp_;
-  RobotState::Ptr robot_state_;
-  Command computeCommand_(RobotState& robot_state);
-
-  VisualizationUtils::Ptr vis_;  
-
 
 };
 
--- main/src/vtr_path_planning/src/mpc/unicycle_mpc_path_tracker.cpp
+++ main/src/vtr_path_planning/src/mpc/unicycle_mpc_path_tracker.cpp
@@ -34,199 +34,104 @@ Eigen::Vector2d saturateVel(const Eigen::Vector2d& applied_vel, double v_lim, do
 }
 
 // Configure the class as a ROS2 node, get configurations from the ros parameter server
-auto UnicycleMPCPathTracker::Config::fromROS(const rclcpp::Node::SharedPtr& node, const std::string& prefix) -> Ptr {
-  auto config = std::make_shared<Config>();
-
-  auto base_config = std::static_pointer_cast<BasePathPlanner::Config>(config);
-  *base_config =  *BasePathPlanner::Config::fromROS(node, prefix);
+auto UnicycleMPCPathTracker::Config::loadConfig(UnicycleMPCPathTracker::Config::Ptr config, 
+		           const rclcpp::Node::SharedPtr& node,
+                           const std::string& prefix)->void{
+  config->q_x   = node->declare_parameter<double>(prefix + ".mpc.q_x", config->q_x);
+  config->q_y   = node->declare_parameter<double>(prefix + ".mpc.q_y", config->q_y);
+  config->q_th  = node->declare_parameter<double>(prefix + ".mpc.q_th", config->q_th);
+  config->r1    = node->declare_parameter<double>(prefix + ".mpc.r1", config->r1);
+  config->r2    = node->declare_parameter<double>(prefix + ".mpc.r2", config->r2);
+  config->racc1 = node->declare_parameter<double>(prefix + ".mpc.racc1", config->racc1);
 
   // MPC Configs:
   // SPEED SCHEDULER PARAMETERS
-  config->planar_curv_weight = node->declare_parameter<double>(prefix + ".speed_scheduler.planar_curv_weight", config->planar_curv_weight);
-  config->profile_curv_weight = node->declare_parameter<double>(prefix + ".speed_scheduler.profile_curv_weight", config->profile_curv_weight);
-  config->eop_weight = node->declare_parameter<double>(prefix + ".speed_scheduler.eop_weight", config->eop_weight);
-  config->min_vel = node->declare_parameter<double>(prefix + ".speed_scheduler.min_vel", config->min_vel);
+  config->q_x   = node->declare_parameter<double>(prefix + ".mpc.q_x", config->q_x);
+  config->q_y   = node->declare_parameter<double>(prefix + ".mpc.q_y", config->q_y);
+  config->q_th  = node->declare_parameter<double>(prefix + ".mpc.q_th", config->q_th);
+  config->r1    = node->declare_parameter<double>(prefix + ".mpc.r1", config->r1);
+  config->r2    = node->declare_parameter<double>(prefix + ".mpc.r2", config->r2);
+  config->racc1 = node->declare_parameter<double>(prefix + ".mpc.racc1", config->racc1);
+  config->racc2 = node->declare_parameter<double>(prefix + ".mpc.racc2", config->racc2);
+}
 
-  // CONTROLLER PARAMS
-  config->extrapolate_robot_pose = node->declare_parameter<bool>(prefix + ".mpc.extrapolate_robot_pose", config->extrapolate_robot_pose);
-  config->mpc_verbosity = node->declare_parameter<bool>(prefix + ".mpc.mpc_verbosity", config->mpc_verbosity);
-  config->forward_vel = node->declare_parameter<double>(prefix + ".mpc.forward_vel", config->forward_vel);
-  config->max_lin_vel = node->declare_parameter<double>(prefix + ".mpc.max_lin_vel", config->max_lin_vel);
-  config->max_ang_vel = node->declare_parameter<double>(prefix + ".mpc.max_ang_vel", config->max_ang_vel);
-  config->max_lin_acc = node->declare_parameter<double>(prefix + ".mpc.max_lin_acc", config->max_lin_acc);
-  config->max_ang_acc = node->declare_parameter<double>(prefix + ".mpc.max_ang_acc", config->max_ang_acc);
-  config->robot_linear_velocity_scale = node->declare_parameter<double>(prefix + ".mpc.robot_linear_velocity_scale", config->robot_linear_velocity_scale);
-  config->robot_angular_velocity_scale = node->declare_parameter<double>(prefix + ".mpc.robot_angular_velocity_scale", config->robot_angular_velocity_scale);
+auto UnicycleMPCPathTracker::Config::fromROS(const rclcpp::Node::SharedPtr& node, const std::string& prefix) -> Ptr {
+  auto config = std::make_shared<Config>();
+  auto base_config = std::static_pointer_cast<BaseMPCPathTracker::Config>(config);
+  *base_config =  *BaseMPCPathTracker::Config::fromROS(node, prefix);
+  loadConfig(config, node, prefix);
 
-  // MISC
-  config->command_history_length = node->declare_parameter<int>(prefix + ".mpc.command_history_length", config->command_history_length);
+  CLOG(DEBUG, "cbit.control") << "Unicycle MPC parameters: "
+      << "q_lat: " << config->q_x
+      << ", q_lon: " << config->q_y
+      << ", q_th: " << config->q_th
+      << ", r1: " << config->r1
+      << ", r2: " << config->r2
+      << ", racc1: " << config->racc1
+      << ", racc2: " << config->racc2
+      << ", alpha: " << config->alpha;
 
   return config;
 }
 
-
-// Declare class as inherited from the BasePathPlanner
+// Declare class as inherited from the BaseMPCPathTracker 
 UnicycleMPCPathTracker::UnicycleMPCPathTracker(const Config::ConstPtr& config,
                                const RobotState::Ptr& robot_state,
                                const tactic::GraphBase::Ptr& graph,
                                const Callback::Ptr& callback)
-    : BasePathPlanner(config, robot_state, graph, callback), config_(config), solver_{config_->mpc_verbosity}, robot_state_{robot_state} {
-  applied_vel_ << 0,
-                  0;
-  vel_history.reserve(config_->command_history_length);
-  for (int i = 0; i < config_->command_history_length; i++)
-  {
-    vel_history.push_back(applied_vel_);
-  }
-
-  vis_ = std::make_shared<VisualizationUtils>(robot_state->node.ptr());
-
+    : BaseMPCPathTracker(config, robot_state, graph, callback), 
+    config_(config), 
+    solver_{config_->mpc_verbosity}{
+      CLOG(DEBUG, "cbit.control") << "Constructed Unicycle tracker";
 }
-
 
 UnicycleMPCPathTracker::~UnicycleMPCPathTracker() {}
 
-auto UnicycleMPCPathTracker::computeCommand(RobotState& robot_state) -> Command {
-  auto raw_command = computeCommand_(robot_state);
-  
-  Eigen::Vector2d output_vel = {raw_command.linear.x, raw_command.angular.z};
+void UnicycleMPCPathTracker::loadMPCConfig(
+    CasadiUnicycleMPC::Config::Ptr mpc_config, Eigen::Matrix<double, 6, 1> w_p_r_in_r, Eigen::Vector2d applied_vel) {
+  // Set the MPC parameters based on the configuration
+  mpc_config->VF           = config_->forward_vel;
+  mpc_config->vel_max(0)   = config_->max_lin_vel;
+  mpc_config->vel_min(0)   = -config_->max_lin_vel;
+  mpc_config->vel_max(1)   = config_->max_ang_vel;
+  mpc_config->vel_min(1)   = -config_->max_ang_vel;
+  mpc_config->previous_vel = {-w_p_r_in_r(0, 0), -w_p_r_in_r(5,0)};
 
-  // Apply robot motor controller calibration scaling factors if applicable
-  output_vel(0) = output_vel(0) * config_->robot_linear_velocity_scale;
-  output_vel(1) = output_vel(1) * config_->robot_angular_velocity_scale;
-
-  // If required, saturate the output velocity commands based on the configuration limits
-  CLOG(DEBUG, "cbit.control") << "Saturating the velocity command if required";
-  Eigen::Vector2d saturated_vel = saturateVel(output_vel, config_->max_lin_vel, config_->max_ang_vel);
-  CLOG(INFO, "cbit.control") << "The Saturated linear velocity is:  " << saturated_vel(0) << " The angular vel is: " << saturated_vel(1);
-  
-  Command command;
-  command.linear.x = saturated_vel(0);
-  command.angular.z = saturated_vel(1);
-  prev_vel_stamp_ = robot_state.node->get_clock()->now().nanoseconds();
-  applied_vel_ = saturated_vel;
-
-  // Store the result in memory so we can use previous state values to re-initialize and extrapolate the robot pose in subsequent iterations
-  vel_history.erase(vel_history.begin());
-  vel_history.push_back(applied_vel_);
-
-  CLOG(INFO, "cbit.control")
-    << "Final control command: [" << command.linear.x << ", "
-    << command.linear.y << ", " << command.linear.z << ", "
-    << command.angular.x << ", " << command.angular.y << ", "
-    << command.angular.z << "]";
-  
-  return command;
+  // Set the MPC costs based on if we're reversing or not
+  mpc_config->Q_y    = config_->q_x;
+  mpc_config->Q_y    = config_->q_y;
+  mpc_config->Q_th   = config_->q_th;
+  mpc_config->R1     = config_->r1; 
+  mpc_config->R2     = config_->r2;
+  mpc_config->Acc_R1 = config_->racc1;
+  mpc_config->Acc_R2 = config_->racc2; 
 }
 
+CasadiMPC::Config::Ptr UnicycleMPCPathTracker::getMPCConfig(Eigen::Matrix<double, 6, 1> w_p_r_in_r, Eigen::Vector2d applied_vel, const bool) {
+  auto mpc_config = std::make_shared<CasadiUnicycleMPC::Config>();
+  loadMPCConfig(mpc_config, w_p_r_in_r, applied_vel);
+  return mpc_config;
+}
 
-// Generate twist commands to track the planned local path (function is called at the control rate)
-auto UnicycleMPCPathTracker::computeCommand_(RobotState& robot_state) -> Command {
-  auto& chain = robot_state.chain.ptr();
-  if (!chain->isLocalized()) {
-    CLOG(WARNING, "cbit.control") << "Robot is not localized, commanding the robot to stop";
-    return Command();
-  }
+bool UnicycleMPCPathTracker::isMPCStateValid(CasadiMPC::Config::Ptr, const tactic::Timestamp& ){
+  return true;
+}
 
-  // retrieve the transform info from the localization chain for the current robot state
-  const auto [stamp, w_p_r_in_r, T_p_r, T_w_p, T_w_v_odo, T_r_v_odo, curr_sid] = getChainInfo(*chain);
+void UnicycleMPCPathTracker::loadMPCPath(CasadiMPC::Config::Ptr mpcConfig, const lgmath::se3::Transformation& T_w_p,
+                         const lgmath::se3::Transformation& T_p_r_extp,
+                         const double state_p,
+                         RobotState& robot_state,
+                         const tactic::Timestamp& t) {
+  auto mpc_config = std::static_pointer_cast<CasadiUnicycleMPC::Config>(mpcConfig);
+  BaseMPCPathTracker::loadMPCPath(mpcConfig, T_w_p, T_p_r_extp, state_p, robot_state, t);
+}
 
-  // Store the current robot state in the robot state path so it can be visualized
-  auto T_w_r = T_w_p * T_p_r;
-
-  CasadiUnicycleMPC::Config mpcConfig;
-  mpcConfig.vel_max = {config_->max_lin_vel, config_->max_ang_vel};
-
-
-    
-  // Schedule speed based on path curvatures + other factors
-  // TODO refactor to accept the chain and use the curvature of the links
-  mpcConfig.VF = ScheduleSpeed(chain, {config_->forward_vel, config_->min_vel, config_->planar_curv_weight, config_->profile_curv_weight, config_->eop_weight, 7});
-
-
-  // EXTRAPOLATING ROBOT POSE INTO THE FUTURE TO COMPENSATE FOR SYSTEM DELAYS
-  auto curr_time = stamp;
-  auto T_p_r_extp = T_p_r;
-  if (config_->extrapolate_robot_pose) {
-    curr_time = robot_state.node->now().nanoseconds();  // always in nanoseconds
-    auto dt = static_cast<double>(curr_time - stamp) * 1e-9 - 0.05;
-    if (fabs(dt) > 0.25) { 
-      CLOG(WARNING, "cbit") << "Pose extrapolation was requested but the time delta is " << dt << "s.\n"
-            << "Ignoring extrapolation requestion. Check your time sync!";
-      dt = 0;
-    }
-
-    CLOG(DEBUG, "cbit.debug") << "Robot velocity Used for Extrapolation: " << -w_p_r_in_r.transpose() << " dt: " << dt << std::endl;
-    Eigen::Matrix<double, 6, 1> xi_p_r_in_r(-dt * w_p_r_in_r);
-    T_p_r_extp = T_p_r * tactic::EdgeTransform(xi_p_r_in_r);
-
-    CLOG(DEBUG, "cbit.debug") << "New extrapolated pose:"  << T_p_r_extp;
-  }
-
-  lgmath::se3::Transformation T0 = T_p_r_extp;
-  mpcConfig.T0 = tf_to_global(T0);
-
-  CLOG(DEBUG, "cbit.control") << "Last velocity " << w_p_r_in_r << " with stamp " << stamp;
-
-  double state_p = findRobotP(T_w_p * T_p_r_extp, chain).second;
-
-  std::vector<double> p_rollout;
-  for(int j = 1; j < mpcConfig.N+1; j++){
-    p_rollout.push_back(state_p + j*mpcConfig.VF*mpcConfig.DT);
-  }
-
-  mpcConfig.reference_poses.clear();
-  auto referenceInfo = generateHomotopyReference(p_rollout, chain);
-  for(const auto& Tf : referenceInfo.poses) {
-    mpcConfig.reference_poses.push_back(tf_to_global(T_w_p.inverse() *  Tf));
-    CLOG(DEBUG, "test") << "Target " << tf_to_global(T_w_p.inverse() *  Tf);
-  }
-
-  mpcConfig.up_barrier_q = referenceInfo.barrier_q_max;
-  mpcConfig.low_barrier_q = referenceInfo.barrier_q_min;
-  
-  mpcConfig.previous_vel = {-w_p_r_in_r(0, 0), -w_p_r_in_r(5, 0)};
-  
-
-  // Create and solve the casadi optimization problem
-  std::vector<lgmath::se3::Transformation> mpc_poses;
-  // return the computed velocity command for the first time step
-  Command command;
-  std::vector<Eigen::Vector2d> mpc_velocities;
-  try {
-    CLOG(INFO, "cbit.control") << "Attempting to solve the MPC problem";
-    auto mpc_res = solver_.solve(mpcConfig);
-    
-    for(int i = 0; i < mpc_res["pose"].columns(); i++) {
-      const auto& pose_i = mpc_res["pose"](casadi::Slice(), i).get_elements();
-      mpc_poses.push_back(T_w_p * tf_from_global(pose_i[0], pose_i[1], pose_i[2]));
-    }
-
-    CLOG(INFO, "cbit.control") << "Successfully solved MPC problem";
-    const auto& mpc_vel_vec = mpc_res["vel"](casadi::Slice(), 0).get_elements();
-
-    command.linear.x = mpc_vel_vec[0];
-    command.angular.z = mpc_vel_vec[1];
-
-    // Get all the mpc velocities 
-    for (int i = 0; i < mpc_res["vel"].columns(); i++) {
-      const auto& vel_i = mpc_res["vel"](casadi::Slice(), i).get_elements();
-      mpc_velocities.emplace_back(vel_i[0], vel_i[1]);
-    }
-
-  } catch(std::exception &e) {
-    CLOG(WARNING, "cbit.control") << "casadi failed! " << e.what() << " Commanding to Stop the Vehicle";
-    return Command();
-  }
-
-  vis_->publishMPCRollout(mpc_poses, curr_time, mpcConfig.DT);
-  vis_->publishReferencePoses(referenceInfo.poses, curr_time);
-
-
-  CLOG(INFO, "cbit.control") << "The linear velocity is:  " << command.linear.x << " The angular vel is: " << command.angular.z;
-
-  return command;
-  
+std::map<std::string, casadi::DM> UnicycleMPCPathTracker::callSolver(CasadiMPC::Config::Ptr config) {
+  std::map<std::string, casadi::DM> result;
+  CLOG(INFO, "cbit.control") << "Attempting to solve the MPC problem";
+  result = solver_.solve(*config);
+  CLOG(INFO, "cbit.control") << "Solver called";
+  return result;
 }
 
 }  // namespace path_planning
